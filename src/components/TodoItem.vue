ca7256310d5b16db77349eb7799bc19129ab231e
gabriel-RM95 <gabriel.rodriguezm95@gmail.com>
2023-09-15 09:32:21 -0400

    Vue.js 62

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,39 @@
+<template>
+    
+</template>
+
+<script>
+import { mapMutations, mapActions } from 'vuex'
+    export default {
+        props: {
+            todo: {
+                type: Object,
+                required: true 
+            },
+        },
+        methods: {
+            ...mapActions({
+                _updateTodoStatus: 'todos/updateTodoStatus',
+                _removeTodo: 'todos/removeTodo'
+            }),
+            ...mapMutations('todo', ['setTodo']),
+            goToUpdateTodo(todo) {
+                this.setTodo(todo);
+                this.$router.push({
+                    name: 'todos-update',
+                    params: {id: todo.id}
+                });
+            },
+            updateTodoStatus(todo) {
+                this._updateTodoStatus(todo);
+            },
+            removeTodo(id){
+                this._removeTodo(id);
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
